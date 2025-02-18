e46e7a3ef31a9b2a51a51e62e97faf463bc5cec2
Jun Jie Lin <junjie@vitrion.nl>
2025-02-18 09:52:55 +0100

    Done

---
+++ src/components/MouseTracker.vue
@@ -0,0 +1,14 @@
+<!-- MouseTracker.vue -->
+<template>
+    <div class="p-4 border rounded shadow-lg">
+      <h2 class="text-lg font-bold">Mouse Tracker</h2>
+      <p>X: {{ x }}, Y: {{ y }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { useMouse } from '@vueuse/core';
+  
+  const { x, y } = useMouse();
+  </script>
+  
