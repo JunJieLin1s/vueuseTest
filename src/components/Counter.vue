45faee8d7224144fe17007621ce54c4f251ffad8
JunJieLin1s <junjie@vitrion.nl>
2025-02-18 09:58:41 +0100

    Update Counter.vue

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -8,7 +8,7 @@
     </div>
   </template>
   
-  <script setup>
+  <script setup lang="ts">
   import { useLocalStorage } from '@vueuse/core';
   
   const count = useLocalStorage('vueuse-counter', 0);
